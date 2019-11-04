a032a87f7f8202883b4c31798ff7b0dedaa331fd
Shadow Smith <shadow@businesscreditreports.com>
2019-11-04 14:56:32 -0600

    Add change-password page

---
+++ nuxt/pages/change-password.vue
@@ -0,0 +1,13 @@
+<template>
+  <SPasswordChangeForm />
+</template>
+
+<script>
+import SPasswordChangeForm from "../components/molecules/SPasswordChangeForm/SPasswordChangeForm";
+
+export default {
+  components: {
+    SPasswordChangeForm
+  }
+};
+</script>
